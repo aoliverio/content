f9d2968501a0c1607d5aa6d11b23d5e0a38a73d5
Antonio Oliverio <antonio.oliverio@gmail.com>
2016-03-23 12:52:05 +0100

    Update AppController.php

--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -3,8 +3,20 @@
 namespace Content\Controller;
 
 use App\Controller\AppController as BaseController;
+use Cake\Core\Configure;
 
-class AppController extends BaseController
-{
+class AppController extends BaseController {
+
+    public function initialize() {
+        parent::initialize();
+
+        /**
+         * Define Content settings
+         */
+        Configure::write('Content', [
+            'defaultUsersTable' => 'Users',
+            'defaultRolesTable' => 'Roles'
+        ]);
+    }
 
 }
