cf7411bf967b835757af03997a6b62925fc7e3bf
Antonio Oliverio <antonio.oliverio@gmail.com>
2016-04-11 12:21:50 +0200

    Updated AppController.php

--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -3,6 +3,7 @@
 namespace Content\Controller;
 
 use App\Controller\AppController as BaseController;
+use Cake\ORM\TableRegistry;
 use Cake\Core\Configure;
 
 class AppController extends BaseController {
@@ -15,8 +16,26 @@ class AppController extends BaseController {
          */
         Configure::write('Content', [
             'defaultUsersTable' => 'Users',
-            'defaultRolesTable' => 'Roles'
+            'defaultRolesTable' => 'Roles',
+            'defaultSiteId' => 1
         ]);
+
+        /**
+         * Manage site selector
+         */
+        if ($this->request->is(['patch', 'post', 'put']))
+            if (isset($this->request->data['current_site_id']))
+                $this->request->session()->write('Content.currentSiteId', intval($this->request->data['current_site_id']));
+
+        $this->CmsSites = TableRegistry::get('Content.CmsSites');
+        $sites = $this->CmsSites->find('all')->toArray();
+        $site_selector = [];
+        foreach ($sites as $row):
+            $site_selector[$row->id] = $row->name;
+            if (!$this->request->session()->check('Content.currentSiteId'))
+                $this->request->session()->write('Content.currentSiteId', $row->id);
+        endforeach;
+        $this->set(compact('site_selector'));
     }
 
 }
